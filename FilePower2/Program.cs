577f8e21dabf70dc6564f3cbf0c13501f8980a94
Leonardo Henrique <leonardojose@netcabo.pt>
2024-05-10 20:27:42 +0100

    Implementação do código

--- FilePower2/Program.cs
+++ FilePower2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace FilePower2
 {
@@ -6,7 +8,29 @@ namespace FilePower2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //take the argument for the file/path
+            string file = args[0];
+
+            //variable needed for the while loop
+            bool readingInput = true;
+
+            //StreamWriter for continuous writing
+            //no closing the file needed because of the "using" keyword
+            using StreamWriter sw = new StreamWriter(file);
+
+            while (readingInput)
+            {
+                string userInput = Console.ReadLine();
+                if (userInput == "")
+                {
+                    break;
+                }
+                else
+                {
+                    //writes the current input in the designated fil
+                    sw.WriteLine(userInput);
+                }
+            }
         }
     }
 }
